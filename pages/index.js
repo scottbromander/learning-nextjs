1b4b982fb7f79f221345e56cd1622b75ecdbac61
Scott Bromander <scottbromander@gmail.com>
2020-05-13 20:35:19 -0500

    adds file reading from MD files and moment for date formatting

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import moment from 'moment';
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -13,6 +15,30 @@ export default function Home() {
           Developer out of Kansas City. But hopefully more west one day soon.
         </p>
       </section>
+      <section className={utilStyles.headingMd}>…</section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title.toUpperCase()}
+              <br />
+              {id.toLowerCase()}
+              <br />
+              {moment(date).format('ll')}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
